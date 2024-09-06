7c077ab365d8c5582fb39c69128881299602219e
habiba-226 <ahabiba402@gmail.com>
2024-09-06 14:17:09 +0300

    update and save options

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,7 +5,7 @@ import {
   updateQuantity,
   updateDeliveryOption
 } from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -18,13 +18,8 @@ export function renderOrderSummary(){
     cart.forEach((cartItem) => {
       const productId = cartItem.productId;
 
-      let matchingProduct;
+      const matchingProduct = getProduct(productId);
 
-      products.forEach((product) => {
-        if (product.id === productId) {
-          matchingProduct = product;
-        }
-      });
 
       const deliveryOptionId = cartItem.deliveryOptionId;
 
@@ -38,7 +33,6 @@ export function renderOrderSummary(){
 
       const today = dayjs();
       const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
-
       const dateString = deliveryDate.format(
         'dddd, MMMM D'
       );
@@ -61,18 +55,24 @@ export function renderOrderSummary(){
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
+
             <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
+               <span> Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
+            </span>
+          <span class="update-quantity-link link-primary js-update-link"
+              data-product-id="${matchingProduct.id}">
+              Update
+            </span>
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}" type="number" value="${cartItem.quantity}" min="1" max="99">
+            <span class="save-quantity-link link-primary js-save-link"
+              data-product-id="${matchingProduct.id}">
+              Save
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
           </div>
+        </div>
 
           <div class="delivery-options">
             <div class="delivery-options-title">
