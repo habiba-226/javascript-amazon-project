b3093fb9aa1c5ef9aa860a7cd108ec44e9417562
habiba-226 <ahabiba402@gmail.com>
2024-09-06 14:47:37 +0300

    save data and generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -10,7 +10,7 @@ import {formatCurrency} from '../utils/money.js';
 
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 
 export function renderOrderSummary(){
     let cartSummaryHTML = '';
@@ -23,13 +23,7 @@ export function renderOrderSummary(){
 
       const deliveryOptionId = cartItem.deliveryOptionId;
 
-      let deliveryOption;
-
-      deliveryOptions.forEach((option) => {
-        if(option.id === deliveryOptionId){
-          deliveryOption = option;
-        }
-      });
+      const deliveryOption =getDeliveryOption(deliveryOptionId);
 
       const today = dayjs();
       const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
@@ -146,6 +140,7 @@ export function renderOrderSummary(){
       
         document.querySelector('.js-checkout-items')
           .innerHTML = `${cartQuantity} items`;
+  
       }
 
     updateCartQuantity();
